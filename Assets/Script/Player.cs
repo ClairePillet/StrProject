2a02c67fa7929846444102144376851a42bfe14c
Claire PILLET <clairenatacha.pillet@free.fr>
2018-05-14 00:08:29 +0200

    Ajout de Base
    Ajout de class: Player, Ressource;
    Ajout de l'uml

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Script
+{
+    class Player
+    {
+        private int id;
+        private string name;
+        private bool isReal;
+        private int popMax;
+        private int popularity;
+        private bool destructable;
+        private bool isAlive;
+
+        public Player(int id, string name,bool isReal, int popMax, int popularity, bool destructable)
+        {
+            this.id = id;
+            this.name = name;
+            this.isReal = isReal;
+            this.popMax = popMax;
+            this.popularity = popularity;
+            this.destructable = destructable;
+            this.isAlive = true;
+
+        }
+        public int getId()
+        {
+            return id;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public bool getIsReal()
+        {
+            return isReal;
+        }
+        public void setIsReal(bool isReal)
+        {
+            this.isReal = isReal;
+        }
+        public bool getGestructable()
+        {
+            return destructable;
+        }
+        public bool getIsAlive()
+        {
+            return isAlive;
+        }
+        public void setIsAlive(bool isReal)
+        {
+            this.isAlive = isReal;
+        }
+        public int getPopMac()
+        {
+            return popMax;
+        }
+        public void setPopMax(int popMax)
+        {
+            this.popMax = popMax;
+        }
+        public int getPopularity()
+        {
+            return popularity;
+        }
+        public void setPopularity(int popularity)
+        {
+            this.popularity = popularity;
+        }
+    }
+}
---
+++ Assets/Script/Ressource.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ressource {
+
+    private int id;
+    private int idPlayer;
+    private int water;
+    private int scrap;
+    private int food;
+    private int energy;
+
+
+    public Ressource(int idPlayer)
+    {
+        this.idPlayer = idPlayer;
+    }
+    public int getId()
+    {
+        return id;
+    }
+    public int getIdPlayer()
+    {
+        return idPlayer;
+    }
+
+    public Dictionary<string,int> getRessource(){
+        Dictionary<string,int> dic= new Dictionary<string, int>();
+        dic.Add("water", water);
+        dic.Add("scrap", scrap);
+        dic.Add("food", food);
+        dic.Add("energy", energy);
+        return dic;
+    }
+    public void setRessource(Dictionary<string, int> dic)
+    {   
+        int  water;
+        int scrap;
+        int food;
+        int energy;
+        dic.TryGetValue("water", out water);
+        dic.TryGetValue("scrap", out scrap);
+        dic.TryGetValue("food", out food);
+        dic.TryGetValue("energy", out energy);
+        this.water += water;
+        this.scrap += scrap;
+        this.food += food;
+        this.energy += energy;
+    }
+
+}
